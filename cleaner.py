deddf0d033baef4ff9afd5cf327cac6a5d22a5b5
Sukesh Shenoy <shenoy.s.sukesh@gmail.com>
2019-11-24 16:00:42 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -22,27 +22,30 @@ class cleaning(emojis):
   def basic_clean(self, sentence):
     p.set_options(p.OPT.URL, p.OPT.SMILEY)
     tmp=p.clean(sentence)
-    emoji_list=self.emojis_sub
-    emoji_list.update({"\s\s+":" ",r"via @":"@"})
-    for key,val in emoji_list.items():
-      tmp=val.join(tmp.split(key))
+    tmp=re.sub(r"via @","@",tmp)
     return(tmp) 
 
   def word_clean(self, sentence):
     p.set_options(p.OPT.HASHTAG,p.OPT.MENTION)
     tmp=p.clean(sentence)
+    emoji_list=self.emojis_sub_word
+    emoji_list.update({"\s\s+":" "})
+    for key,val in emoji_list.items():
+      tmp=val.join(tmp.split(key))
     return([_.lower() for _ in (re.split(r'[`\-=~!$%^&*()_+\[\]{};\\\:"|<,./<>? ]',tmp)) if _ !=""])
 
   def sentence_clean(self, sentence):
     p.set_options(p.OPT.MENTION)
     tmp=p.clean(sentence)
-    return(re.sub("#","",tmp))
+    emoji_list=self.emojis_sub_sentence
+    emoji_list.update({"#":"","\s\s+":" "})
+    for key,val in emoji_list.items():
+      tmp=val.join(tmp.split(key))
+    return(tmp)
   
   def complete_cleaner(self):
     for _ in self.basic_clean_tweets:
       _.words=(self.word_clean(_.clean_text))
       tmp=self.sentence_clean(_.clean_text)
-      _.clean_text=(" ".join(self.word_clean(tmp)))
+      _.clean_text=(" ".join([_.lower() for _ in (re.split(r'[`\-=~!$%^&*()_+\[\]{};\\\:"|<,/<>? ]',tmp)) if _ !=""]))
       self.clean_tweets.append(_)
-
-
