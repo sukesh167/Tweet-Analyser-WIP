b40a1361e27b8141c3085ea26e383cfcfcc02873
Sukesh Shenoy <shenoy.s.sukesh@gmail.com>
2019-11-25 14:00:52 +0530

    Add files via upload

---
+++ processor.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from cleaner import cleaning
+from tweepy import OAuthHandler, API, Cursor
+from pycorenlp import StanfordCoreNLP
+
+class get_tweets():
+	def __init__(self,tweet):
+		self.tweets=tweet
+		self.created_at=tweet.created_at
+		self.user_name=tweet.user.name
+		self.full_text=tweet.full_text
+		self.words=None
+		self.clean_text=None
+		self.retweet_count=tweet.retweet_count.real
+		self.verified_user=tweet.user.verified
+		self.sentiment=None
+		self.sentiment_value=None
+
+class content():
+	def __init__(self):
+		self.clean_tweets=None
+		self.tweets=[]
+		
+		ACCESS_TOKEN = 'access token'
+		ACCESS_TOKEN_SECRET = 'access token secret'
+		CONSUMER_KEY = 'consumer key'
+		CONSUMER_SECRET = 'comsumer secret'
+		
+		auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+		self.api = API(auth, wait_on_rate_limit=True)
+		self.trends=self.api.trends_place(1)[0]['trends'][:5]	
+
+	def tweet_search(self,query):
+		for tweet in Cursor(self.api.search, q=query, lang="en", 
+							tweet_mode="extended").items():
+			self.tweets.append(get_tweets(tweet))
+		
+	def tweet_clean(self):
+		self.clean_tweets=cleaning(self.tweets).clean_tweets
+
+	def sentiment_analysis(self):
+		nlp = StanfordCoreNLP('http://localhost:9000')
+		for text in self.clean_tweets:
+			result = nlp.annotate(text.clean_text,
+					properties={
+					'annotators': 'sentiment',
+					'outputFormat': 'json',
+					'timeout' : 75000})
+			
+			for s in result["sentences"]:
+				text.sentiment=s["sentiment"]
+				text.sentiment_value=s["sentimentValue"]
+	
+
+
